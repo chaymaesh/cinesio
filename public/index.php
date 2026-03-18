986cfd02631761b5f83dc77845e2e383cbb99d77
unknown <essalhic@SIOPERGAUD.FR>
2026-03-18 14:22:11 +0100

    ajout du css du menu, titre et affichage des films

--- public/index.php
+++ public/index.php
@@ -6,34 +6,41 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="../public/assets/css/style.css">
 
 <?php include '../src/includes/header.php' ?>
 
 
 <?php include '../src/data/data.php' ?>
 
-
+<div class="titre">
 <h1> Catalogue des Films</h1>
-<p><?= count($films)?></p>
-
-
-<?php foreach ($films as $film):?>
-<div class="card">
-
-<span class="badge"><?= strtoupper( mb_substr($film["pays"], 0, 3,) ) ?></span>
-  <img src="<?= $film["image"] ?>" alt="film">
-
-    <div class="card-content">
-        <h3> <?= $film["titre"] ?></h3>
-        <p><?= $film["genre"] ?> - <?= $film["duree"]  ?>min</p>
-        <p> <?= $film["synopsis"] ?></p>       
-    </div>
-
-    <div class="card-action">
-        <a href="#" class="btn">Détails</a>
-    </div>
-
+<p>Il y a actuellement <span> <?= count($films)?></span> films dans le catalogue</p>
 </div>
-<?php endforeach; ?>
+
+
+
+<div class="affichage-card">
+    <?php foreach ($films as $film):?>
+    <div class="card">
+
+        <span class="badge"><?= strtoupper( mb_substr($film["pays"], 0, 3,) ) ?></span>
+        <img src="<?= $film["image"] ?>" alt="film">
+
+            <div class="card-content">
+                <h3> <?= $film["titre"] ?></h3>
+                <p><?= $film["genre"] ?> - <?= $film["duree"]  ?>min</p>
+                <p> <?= $film["synopsis"] ?></p>       
+            </div>
+
+            <div class="card-action">
+                <a href="#" class="btn">Détails</a>
+            </div>
+
+    </div>
+    <?php endforeach; ?>
+</div>
+
+
 
 <?php include '../src/includes/footer.php' ?>
--- src/includes/header.php
+++ src/includes/header.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
-    <link rel="stylesheet" href="style-correction.css">
+    
 </head>
 <body>
 
@@ -13,13 +13,13 @@
 
         <!-- logo-->
 
-        <a href="#" class="logo"><i class="bi bi-film"></i></a>
+        <p class="logo "><i class="bi bi-film"></i> Ciné<span>SIO</span></p>
 
         
         <!-- menu-->
 
         <ul class="nav-links">
-            <li><a href="#" class="link">Accueil</a></li>
+            <li><a href="index.php" class="link active">Accueil</a></li>
             <li><a href="#" class="link">Catalogue</a></li>        
             <li><a href="#"class="link">Contact</a></li>
         </ul>
